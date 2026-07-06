35c569c3561783aa3608d0e3fd025bcb0ed0dcb4
Jonathan0823 <eguinjonathan08@gmail.com>
2026-07-06 14:17:53 +0700

    refactor: delete device_service, inline PC macros, simplify log JSON
    - Remove single-target device_service abstraction, use PC_* macros directly
    
    - Replace hand-rolled log JSON/String concat with snprintf fixed buffers
    
    - Inline wake_timeout_seconds() constant wrapper
    
    -net: -62 lines, -2 files

--- src/services/wake_service.h
+++ src/services/wake_service.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 
-unsigned long wake_timeout_seconds();
 void wake_send_magic(const String& mac, const String& bcast, int port);
 bool wake_is_pc_reachable(const String& ip, int port);
 void wake_start_polling(String chatId, const String& deviceName, const String& ip, int probePort);
